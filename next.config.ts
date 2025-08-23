a5ae1e57a7aee264faca74ff46e3f4e8aa311890
PedroDev12 <tu_correo@ejemplo.com>
2025-08-23 18:48:22 +0200

    cambio URL

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-         protocol: 'http', // Local usa HTTP, no HTTPS
-        hostname: 'https://electrolamastore.onrender.com',
-        port: '3001', // El puerto donde corre tu app local
-         pathname: '/img/**',    // Permite todas las imágenes dentro de /img
+        protocol: 'http', // Local usa HTTP, no HTTPS
+        hostname: 'electrolamastore.onrender.com',
+      
+
       },
       {
         protocol: 'https',
